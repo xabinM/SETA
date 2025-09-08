6fb3c2cf13c93465b3f359c318c53cd6c01bb22f
xabinM <binine2@naver.com>
2025-09-08 10:28:46 +0900

    refactor: 공통 auth 요청 url users->auth로 수정

--- Backend/src/main/java/com/AIce/Backend/auth/controller/AuthController.java
+++ Backend/src/main/java/com/AIce/Backend/auth/controller/AuthController.java
@@ -10,14 +10,13 @@ import com.AIce.Backend.auth.dto.signup.SignupResponse;
 import com.AIce.Backend.auth.dto.signup.Tokens;
 import com.AIce.Backend.auth.service.AuthService;
 import com.AIce.Backend.global.enums.ResponseMessage;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
@@ -52,4 +51,6 @@ public class AuthController {
 
         return ResponseEntity.ok(new ReissueResponse(ResponseMessage.SUCCESS_TOKEN_REISSUE.getMessage(), tokens));
     }
+
+    // maybe 회원 정보 수정, 회원 탈퇴
 }
