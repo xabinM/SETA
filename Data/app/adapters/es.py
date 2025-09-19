ec2ef29c61433db4838717e8bbf6d9c386a0d175
DESKTOP-RBNO6LS\SSAFY <lbh3939@gmail.com>
2025-09-19 16:45:38 +0900

    feat: es 주소 이름 변경

--- Data/app/adapters/es.py
+++ Data/app/adapters/es.py
@@ -6,5 +6,5 @@ def get_es_client() -> Elasticsearch:
     Elasticsearch 클라이언트 반환.
     - ES_HOST 환경변수 기반으로 연결
     """
-    host = os.environ["ES_HOST"]
+    host = os.environ["elasticsearch-url"]
     return Elasticsearch(hosts=[host])
