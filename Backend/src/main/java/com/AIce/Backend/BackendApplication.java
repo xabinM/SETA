23e1787e025c474353e632ae97dd9e0a2554ea4a
yumi <yd10407@naver.com>
2025-09-22 18:26:31 +0900

    fix: set jpa & elasticsearch repositories in backendapplication

--- Backend/src/main/java/com/AIce/Backend/BackendApplication.java
+++ Backend/src/main/java/com/AIce/Backend/BackendApplication.java
@@ -3,9 +3,13 @@ package com.AIce.Backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.AIce.Backend.domain")  // JPA repo 경로
+@EnableElasticsearchRepositories(basePackages = "com.AIce.Backend.domain.dashboard.repository") // ES repo 경로
 @EnableJpaAuditing
 public class BackendApplication {
 
